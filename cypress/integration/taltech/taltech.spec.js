afd43fd14e963e938e56ad5b418c31d6f79c5883
Magnus Muru <magmur@ttu.ee>
2020-11-08 21:11:56 +0200

    Commit for assignment 2

---
+++ cypress/integration/taltech/taltech.spec.js
@@ -0,0 +1,70 @@
+context("Taltech Search", () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from failing the test
+        // this is added due to some unknown bug to Cypress devs that I have
+        // might or might not be due to the website
+        // thanks https://github.com/cypress-io/cypress/issues/987
+        return false
+    })
+
+    beforeEach(() => {
+        cy.visit("");
+    })
+
+    it("user can search for person by their first name", ()=>{
+        cy.get(".main-container .top-search").type("Ago{enter}");
+        cy.get(".contacts__name").contains("Ago Luberg");
+    })
+
+    it("user can search for person by their last name", ()=>{
+        cy.get(".main-container .top-search").type("Luberg{enter}");
+        cy.get(".contacts__name").contains("Ago Luberg");
+    })
+
+    it("user can search for person by their full name", ()=>{
+        cy.get(".main-container .top-search").type("Ago Luberg{enter}");
+        cy.get(".contacts__name").contains("Ago Luberg");
+    })
+
+    it("user can search for person and their position", ()=>{
+        cy.get(".main-container .top-search").type("German Mumma{enter}");
+        cy.get(".t-icon__plus-minus").click();
+        cy.get(".contacts__expand > .contacts__listing > li:nth-child(1)").first().should("have.text", "külalisõppejõud")
+    })
+})
+
+context("Taltech Positions", () => {
+
+    beforeEach(() => {
+        cy.visit("");
+    })
+
+    it("user can search for person and their position", ()=>{
+        cy.get(".main-container .top-search").type("German Mumma{enter}");
+        cy.get(".t-icon__plus-minus").click();
+        cy.get(".contacts__expand > .contacts__listing > li:nth-child(1)").first().should("have.text", "külalisõppejõud")
+    })
+})
+
+context("Taltech Grantee", () => {
+
+    beforeEach(() => {
+        cy.visit("");
+    })
+
+    it("grantee can find relevant information regarding grant using menu plus buttons", ()=>{
+        cy.get(".first-level-menu-item").contains("Tudeng").click();
+        cy.get('.t-menu--icon-wrapp').contains("Finantsinfo").parent().find(".t-btn--plus").click();
+        cy.get('.t-menu--icon-wrapp').contains("Õppetoetused ja stipendiumid").parent().find(".t-btn--plus").click();
+        cy.get('.t-menu--icon-wrapp').contains("Sihtstipendium").click();
+        cy.get(".main-container").find("h3").should("have.text", "Üliõpilase kohustused stipendiumi saamisel:")
+    })
+
+    it("grantee can find relevant information regarding grant clicking relevant links", ()=>{
+        cy.get(".first-level-menu-item").contains("Tudeng").click();
+        cy.get('.t-menu--icon-wrapp').contains("Finantsinfo").click();
+        cy.get('.t-menu--icon-wrapp').contains("Õppetoetused ja stipendiumid").click();
+        cy.get(".links__3col").contains("Sihtstipendium").click();
+        cy.get(".main-container").find("h3").should("have.text", "Üliõpilase kohustused stipendiumi saamisel:")
+    })
+})
